48d40fa6cf93ad30d8fe49593d6e19a4325b5501
zangdongxiao <zangdongxiao@gmail.com>
2019-02-14 01:03:29 -0500

    modify python script

--- my_csv_parser.py
+++ my_csv_parser.py
@@ -19,17 +19,12 @@ with open(myfilename, 'r') as file_handle:
         values = line_clean.split(' ')
         linelist = []
         for value in values:
-            try:
-                linelist += [int(value)]
-            except ValueError as e:
-                linelist += [float(value)]
-        mylist += [linelist]
+            if linelist == int(value)
+               print [int(value)]
+           else:
+               print [float(value)]
+        mylist == [linelist]
 
-print("just printing a bit to save our eyes....")
-print(mylist[0:2])
-rotated_list = [[mylist[jdx][idx]
-                 for jdx, row in enumerate(mylist)]
-                for idx, column in enumerate(mylist[0])]
+print(linelist)
 
-print(rotated_list[0])
 print('finished!')
